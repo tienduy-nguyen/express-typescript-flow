3ac283f4ce45c3b77f135e0a873460e4ed47afcf
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-11 23:50:49 +0100

    Create auth service

---
+++ src/modules/auth/auth.interface.ts
@@ -0,0 +1,14 @@
+import { User } from '@modules/users/user.entity';
+import { Request } from 'express';
+
+export interface IRequestUser extends Request {
+  user: User;
+}
+
+export interface IDataStoredInToken {
+  userId: string;
+}
+export interface ITokenCookie {
+  token: string;
+  expiresIn: string | number;
+}
